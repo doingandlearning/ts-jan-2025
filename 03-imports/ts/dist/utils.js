ab897713c178ff0c549210b962088c8a3d5ee0a8
Kevin Cunningham <kevin@kevincunningham.co.uk>
2025-01-20 12:43:37 +0000

    import/export joys!!

---
+++ 03-imports/ts/dist/utils.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.company = exports.location = void 0;
+const location = "London";
+exports.location = location;
+const company = "BBC";
+exports.company = company;
--- 03-imports/ts/src/app.ts
+++ 03-imports/ts/src/app.ts
@@ -1,5 +1,10 @@
 import fs from "node:fs";
 import * as utils from "./utils";
 import { location } from "./utils";
+import { Company } from "./types";
+
+import data from "./test.json";
+
 console.log(utils);
 console.log(location);
+console.log(data);
